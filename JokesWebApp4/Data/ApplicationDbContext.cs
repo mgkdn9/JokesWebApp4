9e3adc1603ef5431c98a6f344481c1219fcc709b
mgkdn9 <mkohlberg95@gmail.com>
2023-03-12 20:26:48 -0400

    Add project files.

---
+++ JokesWebApp4/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokesWebApp4.Models;
+
+namespace JokesWebApp4.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesWebApp4.Models.Joke> Joke { get; set; } = default!;
+    }
+}
